d80748ad8e99b9ea6786647a2c0be7034acd0985
San Kim <waterfog9580@gmail.com>
2023-11-08 23:25:31 +0900

    :tada: 프로젝트 생성

---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "sidematch-demo"
+include("sidematch-bootstrap-api")
+include("sidematch-infrastructure")
+include("sidematch-application")
+include("sidematch-domain")
+include("sidematch-common")
