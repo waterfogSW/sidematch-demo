053ef6d81c1098e14a5b5bb85e4734f5f9fc150c
San Kim <waterfog9580@gmail.com>
2023-11-12 14:41:01 +0900

    ✨ Maker 생성 Validation 메시지 추가

--- sidematch-domain/src/main/kotlin/com/sidematch/domain/entity/Maker.kt
+++ sidematch-domain/src/main/kotlin/com/sidematch/domain/entity/Maker.kt
@@ -13,30 +13,30 @@ data class Maker(
     @field:Length(min = 1, max = 20, message = "이름은 1자 이상 20자 이하로 입력해주세요.")
     private val name: String,
 
-    @field:Length(max = 2000)
+    @field:Length(max = 2000, message = "소개는 2000자 이하로 입력해주세요.")
     private val introduction: String? = null,
 
-    @field:Length(max = 20)
+    @field:Length(max = 2000, message = "관심사는 2000자 이하로 입력해주세요.")
     private val interest: String? = null,
 
-    @field:ValidURL(nullable = true)
+    @field:ValidURL(nullable = true, message = "프로필 이미지 URL 형식이 올바르지 않습니다.")
     private val profileImageUrl: URL? = null,
 
-    @field:Size(max = 3)
+    @field:Size(max = 3, message = "포지션은 최대 3개까지 선택할 수 있습니다.")
     private val positions: Set<MakerPosition> = emptySet(),
 
     private val skillIds: Set<Ulid> = emptySet(),
 
-    @field:ValidURL(nullable = true)
+    @field:ValidURL(nullable = true, message = "Github URL 형식이 올바르지 않습니다.")
     private val githubUrl: URL? = null,
 
-    @field:ValidURL(nullable = true)
+    @field:ValidURL(nullable = true, message = "포트폴리오 URL 형식이 올바르지 않습니다.")
     private val portfolioUrl: URL? = null,
 
-    @field:Size(max = 10)
+    @field:Size(max = 10, message = "경력은 최대 10개까지 선택할 수 있습니다.")
     private val careerIds: Set<Ulid> = emptySet(),
 
-    @field:Length(max = 100)
+    @field:Length(max = 100, message = "목적은 100자 이하로 입력해주세요.")
     private val purpose: String? = null,
 ) : Entity() {
 
