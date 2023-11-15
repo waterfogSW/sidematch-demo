11eef4c086905e4f4bfcc6fb491e9d1db382006c
San Kim <waterfog9580@gmail.com>
2023-11-15 19:53:47 +0900

    ✨ UseCase, Adapter 어노테이션 추가

---
+++ sidematch-common/src/main/kotlin/com/sidematch/common/annotation/Adapter.kt
@@ -0,0 +1,9 @@
+package com.sidematch.common.annotation
+
+import org.springframework.stereotype.Component
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Component
+annotation class Adapter
