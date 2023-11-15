9d424b298c113552392c998d1140199f5d77cb3f
San Kim <waterfog9580@gmail.com>
2023-11-15 23:44:14 +0900

    ➕ Application 모듈 의존성 추가

---
+++ sidematch-application/build.gradle.kts
@@ -0,0 +1,14 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+val jar: Jar by tasks
+val bootJar: BootJar by tasks
+
+jar.enabled = true
+bootJar.enabled = false
+
+dependencies {
+    implementation(project(":sidematch-common"))
+    implementation(project(":sidematch-domain"))
+
+    implementation("org.springframework.boot:spring-boot")
+}
