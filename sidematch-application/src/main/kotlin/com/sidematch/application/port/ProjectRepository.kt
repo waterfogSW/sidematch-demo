fa1b10ee6c2ea854d6e1c77d8c2c7a91e4421ed2
San Kim <waterfog9580@gmail.com>
2023-11-23 00:01:09 +0900

    ✨ ProjectRepository findById Repository 추가

--- sidematch-application/src/main/kotlin/com/sidematch/application/port/ProjectRepository.kt
+++ sidematch-application/src/main/kotlin/com/sidematch/application/port/ProjectRepository.kt
@@ -1,8 +1,11 @@
 package com.sidematch.application.port
 
+import com.github.f4b6a3.ulid.Ulid
 import com.sidematch.domain.entity.Project
 
 interface ProjectRepository {
 
     fun save(project: Project)
+
+    fun findById(id: Ulid): Project?
 }
